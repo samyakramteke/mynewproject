7c95c387749afa5f0b5f80c45248d96410c9ec9a
Samyak <samyakramteke47@gmail.com>
2026-04-26 10:40:07 +0530

    Updated project files

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
-print("Hello Samyak")
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("yolov8n.pt")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    results = model(frame)
+
+    annotated = results[0].plot()
+    cv2.imshow("YOLO", annotated)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
 
 
 
